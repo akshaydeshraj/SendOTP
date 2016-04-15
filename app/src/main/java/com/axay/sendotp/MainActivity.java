653e5fa2a49b186f0f965b2888ef27a5fb3170f9
Akshay Deshraj <akshay30nov@gmail.com>
2016-04-15 10:35:10 +0530

    demo use of library in application

--- app/src/main/java/com/axay/sendotp/MainActivity.java
+++ app/src/main/java/com/axay/sendotp/MainActivity.java
@@ -2,12 +2,35 @@ package com.axay.sendotp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends AppCompatActivity {
+import com.msg91.axay.sendotp.OtpVerificationListener;
+import com.msg91.axay.sendotp.SendOtpActivity;
+
+public class MainActivity extends AppCompatActivity implements OtpVerificationListener {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SendOtpActivity.start(this);
+    }
+
+    @Override
+    public void onOtpVerificationSuccess(String mobileNumber, String response) {
+        Log.i(TAG, mobileNumber + response);
+    }
+
+    @Override
+    public void onOtpVerificationFailure(Exception exception) {
+        Log.i(TAG, exception.getMessage());
+    }
+
+    @Override
+    public void onInitiationFailed(Exception exception) {
+        Log.i(TAG, exception.getMessage());
     }
 }
